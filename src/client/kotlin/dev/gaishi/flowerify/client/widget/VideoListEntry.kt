a301d2ea37b1c58c31dbd3a8214546086124a286
Gaishi Hirota <mail@gaishi.dev>
2024-08-04 02:17:41 +0900

    feat: add play button

--- src/client/kotlin/dev/gaishi/flowerify/client/widget/VideoListEntry.kt
+++ src/client/kotlin/dev/gaishi/flowerify/client/widget/VideoListEntry.kt
@@ -7,6 +7,7 @@ import net.minecraft.client.MinecraftClient
 import net.minecraft.client.gui.DrawContext
 import net.minecraft.client.gui.Element
 import net.minecraft.client.gui.Selectable
+import net.minecraft.client.gui.widget.ButtonWidget
 import net.minecraft.client.gui.widget.ClickableWidget
 import net.minecraft.client.gui.widget.ElementListWidget
 import net.minecraft.text.OrderedText
@@ -17,9 +18,13 @@ import net.minecraft.util.Language
 @Environment(EnvType.CLIENT)
 class VideoListEntry(private val video: Video): ElementListWidget.Entry<VideoListEntry>() {
     object VideoListEntryConstants {
+        const val TITLE_WIDTH = 250
         const val ELLIPSIS = "..."
     }
-    private val children = ArrayList<ClickableWidget>()
+    private val playButton: ButtonWidget = ButtonWidget.builder(Text.of("Play")) { _ ->
+        println("Play ${video.name}")
+    }.dimensions(0, 0, 30, 20).build()
+    private val buttons = arrayListOf<ClickableWidget>(playButton)
 
     override fun render(
         context: DrawContext,
@@ -35,26 +40,29 @@ class VideoListEntry(private val video: Video): ElementListWidget.Entry<VideoLis
     ) {
         context.drawText(MinecraftClient.getInstance().textRenderer, trimTextToWidth(Text.of(video.name)), x, y, 0xFFFFFFFF.toInt(), true)
         context.drawBorder(x, y, entryWidth, entryHeight, 0xFFFFFFFF.toInt())
+        playButton.x = x + VideoListEntryConstants.TITLE_WIDTH + 4
+        playButton.y = y + (entryHeight - playButton.height) / 2
+        playButton.render(context, mouseX, mouseY, tickDelta)
     }
 
     override fun children(): MutableList<out Element> {
-        return this.children
+        return this.buttons
     }
 
     override fun selectableChildren(): MutableList<out Selectable> {
-        return this.children
+        return this.buttons
     }
 
     companion object {
         fun trimTextToWidth(text: Text): OrderedText? {
             val client = MinecraftClient.getInstance()
             val i: Int = client.textRenderer.getWidth(text as StringVisitable)
-            if (i > 250) {
+            if (i > VideoListEntryConstants.TITLE_WIDTH) {
                 val stringVisitable = StringVisitable.concat(
                     *arrayOf<StringVisitable>(
                         client.textRenderer.trimToWidth(
                             text as StringVisitable,
-                            250 - client.textRenderer.getWidth(VideoListEntryConstants.ELLIPSIS)
+                            VideoListEntryConstants.TITLE_WIDTH - client.textRenderer.getWidth(VideoListEntryConstants.ELLIPSIS)
                         ), StringVisitable.plain(VideoListEntryConstants.ELLIPSIS)
                     )
                 )
